f1de57925dca349039371aadb99de3dc53aeb81d
rawkthethinker <47385943+rawkthethinker@users.noreply.github.com>
2020-04-07 02:55:40 +0200

    updated to support working in github pages

---
+++ precache-manifest.dacdcc255355db218f51cb8bee21e872.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21aaf48fef789cf09ea67915bfeaa43e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2bdc0241deef2e51c3ca",
+    "url": "/static/css/main.5db78a48.chunk.css"
+  },
+  {
+    "revision": "ddf5e68d057731f06a87",
+    "url": "/static/js/2.00216c83.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.00216c83.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2bdc0241deef2e51c3ca",
+    "url": "/static/js/main.a103c735.chunk.js"
+  },
+  {
+    "revision": "228fa098814da32aa5c2",
+    "url": "/static/js/runtime-main.e1daef6b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
